f28f8abcde098b38ade31074854f52ad8d73e1f7
Quentin W <quentin.wachowiak@wanadoo.fr>
2017-02-10 09:22:32 -0500

    Démarche voteServices

--- voteapp-webapp/src/main/java/fr/sgr/formation/voteapp/vote/services/VotesServices.java
+++ voteapp-webapp/src/main/java/fr/sgr/formation/voteapp/vote/services/VotesServices.java
@@ -9,9 +9,12 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import fr.sgr.formation.voteapp.election.modele.Election;
+import fr.sgr.formation.voteapp.utilisateurs.modele.Utilisateur;
 import fr.sgr.formation.voteapp.utilisateurs.services.DroitAccesException;
-import fr.sgr.formation.voteapp.vote.modele.Vote;
+import fr.sgr.formation.voteapp.vote.modele.ValeurVote;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class VotesServices {
 
 	@Autowired
@@ -25,7 +28,18 @@ public class VotesServices {
 	 * @throws DroitAccesException
 	 */
 	@Transactional(propagation = Propagation.REQUIRED)
-	public void creerVote(Vote nouveauVote, Election election) {
+	public void creerVote(Utilisateur votant, ValeurVote valeur, Election election) {
+
+		/**
+		 * On commence par vérifier que "votant" est non null. Puis on crée une
+		 * trace (tracesServices.init(...)). Puis on vérifie que l'élection
+		 * existe. Puis on vérifie qu'elle n'est pas cloturée. Puis on vérifie
+		 * que l'utilisateur n'a pas déjà voté. Puis on créer l'objet vote à
+		 * partir de la valeur du vote, du votant et de l'élection. Puis on MAJ
+		 * la trace en disant que ça marche. Puis on ajoute le vote à
+		 * l'élection.
+		 */
+
 		Date today = new Date();
 		// on regarde si la date du jour est avant la date de fin de l'élection.
 		// Si l'élection n'existe pas, on devrait avoir une nullpointer
